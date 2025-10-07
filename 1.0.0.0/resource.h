179c926983c8ba1338c9c9f7cfe78d4e148e3c96
SiyuanX237 <13865507815@163.com>
2025-10-07 22:34:41 +0800

    Add files via upload

---
+++ 1.0.0.0/resource.h
@@ -0,0 +1,48 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Window's Handle.rc 使用
+//
+#define IDI_ICON1		11
+//菜单项
+#define ID_MENU_MAX		101
+#define ID_MENU_NORMAL	102
+#define ID_MENU_MIN		103
+#define ID_MENU_HIDE	104
+#define ID_MENU_ENABLE	105
+#define ID_MENU_DISABLE	106
+#define ID_MENU_TOP		107
+#define ID_MENU_NOTOP	108
+#define ID_MENU_THROUGH	109
+#define ID_MENU_NOTHROUGH	110
+
+//控件ID
+#define ID_BTN_SELECT	1001
+#define ID_BTN_SHAKE	1002
+#define ID_BTN_TRANSPARENT	1003
+#define ID_BTN_POS		1004
+#define ID_BTN_SIZE		1005
+#define ID_BTN_ENABLE	1006
+#define ID_BTN_TOP		1007
+#define ID_BTN_THROUGH	1008
+#define ID_EDIT_Handle	1009
+#define ID_BTN_Title	1010
+#define ID_BTN_Noactivate 1011
+#define ID_BTN_Style	1012
+#define ID_BTN_ExStyle	1013
+#define ID_BTN_Enum		1014
+#define ID_BTN_EnumChild		1015
+#define ID_BTN_SetParent	1016
+#define ID_BTN_Message	1017
+#define ID_BTN_About	1018
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
---
+++ 1.0.0.0/标头.h
@@ -0,0 +1,116 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <windows.h>
+#include <iostream>
+#include "D:\C files\myWin32func.h"
+#include "D:\C files\uiaccess.h"
+#include <windowsx.h>
+#include <psapi.h>
+#pragma comment(lib, "psapi.lib")
+#include <unordered_map>
+#include <commctrl.h>
+#pragma comment(lib, "comctl32.lib")
+#include "resource.h"
+#pragma comment(linker,	"\"/manifestdependency:type='win32' \
+							name='Microsoft.Windows.Common-Controls' \
+							version='6.0.0.0' \
+							processorArchitecture='*' \
+							publicKeyToken='6595b64144ccf1df' \
+							language='*'\"")
+using namespace std;
+
+//自定义消息
+#define WM_TARGET WM_USER+1
+#define WM_POS WM_USER+2
+#define WM_SIZECHANGE WM_USER+3
+#define WM_TRAY WM_USER+4
+
+//全局宏定义
+constexpr int HANDLESIZE = 21;
+
+//窗口风格类
+constexpr struct { DWORD flag; LPCSTR name; } StyleFlags[] = {
+	{ WS_BORDER,		"WS_BORDER" },
+	{ WS_CAPTION,		"WS_CAPTION" },
+	{ WS_CHILD,			"WS_CHILD" },
+	{ WS_CHILDWINDOW,	"WS_CHILDWINDOW" },
+	{ WS_CLIPCHILDREN,	"WS_CLIPCHILDREN" },
+	{ WS_CLIPSIBLINGS,	"WS_CLIPSIBLINGS" },
+	{ WS_DISABLED,		"WS_DISABLED" },
+	{ WS_DLGFRAME,		"WS_DLGFRAME" },
+	{ WS_GROUP,			"WS_GROUP" },
+	{ WS_HSCROLL,		"WS_HSCROLL" },
+	{ WS_ICONIC,		"WS_ICONIC" },
+	{ WS_MAXIMIZE,		"WS_MAXIMIZE" },
+	{ WS_MAXIMIZEBOX,	"WS_MAXIMIZEBOX" },
+	{ WS_MINIMIZE,		"WS_MINIMIZE" },
+	{ WS_MINIMIZEBOX,	"WS_MINIMIZEBOX" },
+	{ WS_OVERLAPPED,	"WS_OVERLAPPED" },//0
+	{ WS_OVERLAPPEDWINDOW,	"WS_OVERLAPPEDWINDOW" },
+	{ WS_POPUP,			"WS_POPUP" },
+	{ WS_POPUPWINDOW,	"WS_POPUPWINDOW" },
+	{ WS_SIZEBOX,		"WS_SIZEBOX" },
+	{ WS_SYSMENU,		"WS_SYSMENU" },
+	{ WS_TABSTOP,		"WS_TABSTOP" },
+	{ WS_THICKFRAME,	"WS_THICKFRAME" },
+	{ WS_TILED,			"WS_TILED" },//0
+	{ WS_TILEDWINDOW,	"WS_TILEDWINDOW" },
+	{ WS_VISIBLE,		"WS_VISIBLE" },
+	{ WS_VSCROLL,		"WS_VSCROLL" },
+};
+constexpr struct { DWORD flag; LPCSTR name; } ExStyleFlags[] = {
+{ WS_EX_ACCEPTFILES,	"WS_EX_ACCEPTFILES" },
+{ WS_EX_APPWINDOW,		"WS_EX_APPWINDOW" },
+{ WS_EX_CLIENTEDGE,		"WS_EX_CLIENTEDGE" },
+{ WS_EX_COMPOSITED,		"WS_EX_COMPOSITED" },
+{ WS_EX_CONTEXTHELP,	"WS_EX_CONTEXTHELP" },
+{ WS_EX_CONTROLPARENT,	"WS_EX_CONTROLPARENT" },
+{ WS_EX_DLGMODALFRAME,	"WS_EX_DLGMODALFRAME" },
+{ WS_EX_LAYERED,		"WS_EX_LAYERED" },
+{ WS_EX_LAYOUTRTL,		"WS_EX_LAYOUTRTL" },
+{ WS_EX_LEFT,			"WS_EX_LEFT" },//0
+{ WS_EX_LEFTSCROLLBAR,	"WS_EX_LEFTSCROLLBAR" },
+{ WS_EX_LTRREADING,		"WS_EX_LTRREADING" },//0
+{ WS_EX_MDICHILD,		"WS_EX_MDICHILD" },
+{ WS_EX_NOACTIVATE,		"WS_EX_NOACTIVATE" },
+{ WS_EX_NOINHERITLAYOUT,"WS_EX_NOINHERITLAYOUT" },
+{ WS_EX_NOPARENTNOTIFY,		"WS_EX_NOPARENTNOTIFY" },
+#if _WIN32_WINNT > _WIN32_WINNT_WINXP
+{ WS_EX_NOREDIRECTIONBITMAP,"WS_EX_NOREDIRECTIONBITMAP" },
+#endif
+{ WS_EX_OVERLAPPEDWINDOW,	"WS_EX_OVERLAPPEDWINDOW" },
+{ WS_EX_PALETTEWINDOW,	"WS_EX_PALETTEWINDOW" },
+{ WS_EX_RIGHT,			"WS_EX_RIGHT" },
+{ WS_EX_RIGHTSCROLLBAR,	"WS_EX_RIGHTSCROLLBAR" },//0
+{ WS_EX_RTLREADING,		"WS_EX_RTLREADING" },
+{ WS_EX_STATICEDGE,		"WS_EX_STATICEDGE" },
+{ WS_EX_TOOLWINDOW,		"WS_EX_TOOLWINDOW" },
+{ WS_EX_TOPMOST,		"WS_EX_TOPMOST" },
+{ WS_EX_TRANSPARENT,	"WS_EX_TRANSPARENT" },
+{ WS_EX_WINDOWEDGE,		"WS_EX_WINDOWEDGE" },
+};
+
+//全局变量区
+extern HWND g_MainHwnd;
+extern HWND g_StaticClassname;
+extern HWND g_EditTitlename;
+extern HWND g_StaticExename;
+extern HWND g_StaticStyle;
+extern HWND g_StaticExStyle;
+extern HWND g_EditTransparent;
+extern HWND g_EditPosX;
+extern HWND g_EditPosY;
+extern HWND g_EditSizeX;
+extern HWND g_EditSizeY;
+extern HWND g_StaticInstance;
+
+//函数声明
+//wstring GetProcessNameFromWindow2(HWND hwnd);
+wstring GetProcessNameFromWindow(HWND hwnd);
+BOOL IsAdmin();
+void ShakeWindow(HWND hWnd);
+bool SetWindowTransparent(HWND &target, INT32 &trans);
+void FillInformation(HWND target);
+void ResetInformation();
+LONG_PTR SetWindowThrough(HWND hWnd, BOOL enabled, LONG_PTR OldexStyle);
