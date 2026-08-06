873e5d1f4c59d45efc12421d54fc83ad06ac79e5
SiyuanX237 <13865507815@163.com>
2026-08-07 01:09:02 +0800

    Add files via upload

---
+++ 1.4.4.0/resource.h
@@ -0,0 +1,71 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Window's Handle.rc 使用
+//
+#define IDI_ICON1		11
+//菜单项
+#define ID_MENU_MAX		101
+#define ID_MENU_NORMAL	102
+#define ID_MENU_MIN		103
+#define ID_MENU_HIDE	104
+#define ID_MENU_ENABLE	105
+#define ID_MENU_DISABLE	106
+#define ID_MENU_TOP		107
+#define ID_MENU_NOTOP	108
+#define ID_MENU_THROUGH	109
+#define ID_MENU_NOTHROUGH	110
+#define ID_MENU_AUTORULEENABLE	111
+#define ID_MENU_AUTORULEDISENABLE	112
+#define ID_MENU_ChainParent	113
+#define ID_MENU_ChainOwner	114
+
+//控件ID
+#define ID_BTN_Select	1001
+#define ID_BTN_Shake	1002
+#define ID_BTN_Transparent	1003
+#define ID_BTN_Pos		1004
+#define ID_BTN_Size		1005
+#define ID_BTN_Enable	1006
+#define ID_BTN_Top		1007
+#define ID_BTN_Through	1008
+#define ID_EDIT_Handle	1009
+#define ID_BTN_Title	1010
+#define ID_BTN_Noactivate 1011
+#define ID_BTN_Style	1012
+#define ID_BTN_ExStyle	1013
+#define ID_BTN_Enum		1014
+#define ID_BTN_SetFollow	1015
+#define ID_BTN_Message	1016
+#define ID_BTN_About	1017
+#define ID_BTN_AutoRule	1018
+#define ID_BTN_Hotkey	1019
+#define ID_BTN_Chain	1020
+
+//快捷键ID
+#define ID_Hotkey_Mainwindow	1
+#define ID_Hotkey_Select		2
+#define ID_Hotkey_SelectParent	3
+#define ID_Hotkey_Transparent	4//暂没用到
+#define ID_Hotkey_Topmost		5
+#define ID_Hotkey_Move			6//暂没用到
+#define ID_Hotkey_Size			7//暂没用到
+
+//定时器ID
+#define ID_Timer_Pos			1
+#define ID_Timer_LSelect		2
+#define ID_Timer_RSelect		3
+#define ID_Timer_LSelectHotkey	4
+#define ID_Timer_RSelectHotkey	5
+#define ID_Timer_Move			6
+#define ID_Timer_Size			7
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
